1d61442e22ae3b9ea0e31056324adab1c10ec0b2
ajn123 <alexander.norton@frontgatetickets.com>
2024-09-03 01:10:08 -0400

    guest profile setup, google maps setup

---
+++ app/Filament/Resources/ClubResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ClubResource\Pages;
+use App\Filament\Resources\ClubResource\RelationManagers;
+use App\Models\Club;
+use Cheesegrits\FilamentGoogleMaps\Fields\Map;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Pages\Page;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ClubResource extends Resource
+{
+    protected static ?string $model = Club::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('website')->prefix('https://'),
+                Forms\Components\TextInput::make('instagram')->prefix('@'),
+                Forms\Components\Select::make('location')->options([
+                    'Virginia',
+                    'Maryland',
+                    'DC'
+                ])->required(),
+                Map::make('location')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListClubs::route('/'),
+            'create' => Pages\CreateClub::route('/create'),
+            'edit' => Pages\EditClub::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Club.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Club extends Model
+{
+    use HasFactory;
+
+
+    protected $guarded = [];
+
+
+    /**
+     * REPLACE THE FOLLOWING ARRAYS IN YOUR Club MODEL
+     *
+     * Replace your existing $fillable and/or $guarded and/or $appends arrays with these - we already merged
+     * any existing attributes from your model, and only included the one(s) that need changing.
+     */
+
+
+    protected $fillable = [
+        'location',
+    ];
+
+    protected $appends = [
+        'location',
+    ];
+
+    /**
+     * ADD THE FOLLOWING METHODS TO YOUR Club MODEL
+     *
+     * The 'latitude' and 'longitude' attributes should exist as fields in your table schema,
+     * holding standard decimal latitude and longitude coordinates.
+     *
+     * The 'location' attribute should NOT exist in your table schema, rather it is a computed attribute,
+     * which you will use as the field name for your Filament Google Maps form fields and table columns.
+     *
+     * You may of course strip all comments, if you don't feel verbose.
+     */
+
+    /**
+     * Returns the 'latitude' and 'longitude' attributes as the computed 'location' attribute,
+     * as a standard Google Maps style Point array with 'lat' and 'lng' attributes.
+     *
+     * Used by the Filament Google Maps package.
+     *
+     * Requires the 'location' attribute be included in this model's $fillable array.
+     *
+     * @return array
+     */
+
+    public function getLocationAttribute(): array
+    {
+        return [
+            "lat" => (float)$this->latitude,
+            "lng" => (float)$this->longitude,
+        ];
+    }
+
+    /**
+     * Takes a Google style Point array of 'lat' and 'lng' values and assigns them to the
+     * 'latitude' and 'longitude' attributes on this model.
+     *
+     * Used by the Filament Google Maps package.
+     *
+     * Requires the 'location' attribute be included in this model's $fillable array.
+     *
+     * @param ?array $location
+     * @return void
+     */
+    public function setLocationAttribute(?array $location): void
+    {
+        if (is_array($location))
+        {
+            $this->attributes['latitude'] = $location['lat'];
+            $this->attributes['longitude'] = $location['lng'];
+            unset($this->attributes['location']);
+        }
+    }
+
+    /**
+     * Get the lat and lng attribute/field names used on this table
+     *
+     * Used by the Filament Google Maps package.
+     *
+     * @return string[]
+     */
+    public static function getLatLngAttributes(): array
+    {
+        return [
+            'lat' => 'latitude',
+            'lng' => 'longitude',
+        ];
+    }
+
+    /**
+     * Get the name of the computed location attribute
+     *
+     * Used by the Filament Google Maps package.
+     *
+     * @return string
+     */
+    public static function getComputedLocation(): string
+    {
+        return 'location';
+    }
+
+}
